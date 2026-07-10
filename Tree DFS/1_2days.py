1b1c5edbae02cebd217a4fc6cae2ff8866272585
jayeshmurthi49-byte <jayeshmurthi49@gmail.com>
2026-07-10 23:33:15 +0530

    1 Days

---
+++ Tree DFS/1_2days.py
@@ -0,0 +1,23 @@
+# Keywords to spot →
+# ✅ "invert binary tree"
+# ✅ "mirror binary tree"
+# ✅ "flip binary tree"
+# ✅ "swap left and right"
+
+# When you see these words →
+# Think Tree DFS! 💡
+# Swap left and right
+# at every node! 
+
+
+# Input:      4
+#            / \
+#           2   7
+#          / \ / \
+#         1  3 6  9
+
+# Output:     4
+#            / \
+#           7   2
+#          / \ / \
+#         9  6 3  1
