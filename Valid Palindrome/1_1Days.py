f53f69a7756ab0b4659225e51f826d2b5057dd22
jayeshmurthi49-byte <jayeshmurthi49@gmail.com>
2026-07-31 00:09:16 +0530

    1_1Days.py

---
+++ Valid Palindrome/1_1Days.py
@@ -0,0 +1,21 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+
+
+def palindrome(s):
+    s = "".join(c.lower() for c in s if c.isalnum())
+    return s == s[::-1]
