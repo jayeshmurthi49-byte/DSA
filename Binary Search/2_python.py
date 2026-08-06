393f48def0ec1741d7fbafe77e404955f6f70de1
jayeshmurthi49-byte <jayeshmurthi49@gmail.com>
2026-08-06 22:11:06 +0530

    updated code

--- Binary Search/2_python.py
+++ Binary Search/2_python.py
@@ -1,26 +1,23 @@
-# 2 D array
+def solution(nums,target):
+    l,r = 0 ,len(nums) - 1
 
-def search(nums,target):
-    left = 0
-    right = len(nums) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
+    while l <= r:
+        mid = (l+r) // 2
 
         if nums[mid] == target:
             return mid
         
-        if nums[left] <= nums[mid]:
-            if nums[left] <= target < nums[mid]:
-                right = mid - 1
+        if nums[l] <= nums[mid]:
+            if nums[l] <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        else:
+            if nums[mid] < target <= nums[r]:
+                l = mid + 1
 
             else:
-                left =  mid + 1
-        else:
-            if nums[mid] < target <= nums[right]:
-                left = mid + 1
-            else:
-                right  = mid - 1
+                r = mid - 1
 
     return - 1
-                
